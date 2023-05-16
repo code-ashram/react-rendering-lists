4939564789452a5971d47bc5473aa739f2644e71
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-05-16 20:23:59 +0300

    task-1 ready

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import { people } from './data.js'
+import { getImageUrl } from './utils.js'
+
+function List({ profession }) {
+  const chemists = people.filter((person) =>
+    person.profession === 'chemist'
+  )
+
+  const everyOneElse = people.filter((person) =>
+    person.profession !== 'chemist'
+  )
+
+  const groupPeople = (personCategory) => personCategory.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  )
+
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemists</h2>
+      <ul className="list">{groupPeople(chemists)}</ul>
+      <h2>Everyone else</h2>
+      <ul className="list">{groupPeople(everyOneElse)}</ul>
+    </article>
+  )
+}
+
+export default List
