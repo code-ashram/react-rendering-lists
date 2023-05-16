d5ea240dbc4d1feb7dab85479f0900f9dc00918b
Andrei Yeutukhou <stalker10eg@mail.ru>
2023-05-16 23:00:13 +0300

    try to optimize task-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
 import './App.css'
-import { people } from './data.js'
-import { getImageUrl } from './utils.js'
+import {people} from './data.js'
+import {getImageUrl} from './utils.js'
+import {Fragment} from "react";
 
-const List = () => {
-  const chemists = people.filter((person) =>
-    person.profession === 'chemist'
-  )
+const CommonList = () => {
+    const chemists = people.filter((person) =>
+        person.profession === 'chemist'
+    )
 
-  const everyOneElse = people.filter((person) =>
-    person.profession !== 'chemist'
-  )
+    const everyOneElse = people.filter((person) =>
+        person.profession !== 'chemist'
+    )
 
-  const groupPeople = (personCategory) => personCategory.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  )
+    const groupPeople = (personCategory) => personCategory.map(person =>
+        <li key={person.id}>
+            <img
+                src={getImageUrl(person)}
+                alt={person.name}
+            />
+            <p>
+                <b>{person.name}:</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
+            </p>
+        </li>
+    )
 
-  return (
-    <article>
-      <h1>Scientists</h1>
-      <h2>Chemists</h2>
-      <ul className="list">{groupPeople(chemists)}</ul>
-      <h2>Everyone else</h2>
-      <ul className="list">{groupPeople(everyOneElse)}</ul>
-    </article>
-  )
+    const List = ({title, source}) => <Fragment>
+        <h2>{title}</h2>
+        <ul className="list">{source}</ul>
+    </Fragment>
+
+    return (
+        <article>
+            <h1>Scientists</h1>
+            <List title={"Chemists"} source={groupPeople(chemists)}/>
+            <List title={"Everyone else"} source={groupPeople(everyOneElse)}/>
+        </article>
+    )
 }
 
-export default List
+export default CommonList
