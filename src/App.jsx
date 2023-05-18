638cb8f6163374f19d32ceadb04496db8ab5a311
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-05-18 19:08:26 +0300

    task-1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import './App.css'
 import {people} from './data.js'
 import {getImageUrl} from './utils.js'
-import {Fragment} from "react";
 
-const CommonList = () => {
-    const chemists = people.filter((person) =>
-        person.profession === 'chemist'
-    )
+const chemists = people.filter((person) => person.profession === 'chemist')
 
-    const everyOneElse = people.filter((person) =>
-        person.profession !== 'chemist'
-    )
+const everyOneElse = people.filter((person) => person.profession !== 'chemist')
 
-    const groupPeople = (personCategory) => personCategory.map(person =>
-        <li key={person.id}>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}:</b>
-                {' ' + person.profession + ' '}
-                known for {person.accomplishment}
-            </p>
-        </li>
-    )
+const ListItem = ({person}) => (
+    <li>
+        <img
+            src={getImageUrl(person)}
+            alt={person.name}
+        />
+        <p>
+            <b>{person.name}:</b>
+            {' ' + person.profession + ' '}
+            known for {person.accomplishment}
+        </p>
+    </li>
+)
 
-    const List = ({title, source}) => <Fragment>
+const List = ({title, list}) => (
+    <>
         <h2>{title}</h2>
-        <ul className="list">{source}</ul>
-    </Fragment>
+        <ul>
+            {list.map((person) => (
+                <ListItem key={person.id} person={person}/>
+            ))}
+        </ul>
+    </>
+)
 
-    return (
-        <article>
-            <h1>Scientists</h1>
-            <List title={"Chemists"} source={groupPeople(chemists)}/>
-            <List title={"Everyone else"} source={groupPeople(everyOneElse)}/>
-        </article>
-    )
-}
+const CommonList = () => (
+    <article>
+        <h1>Scientists</h1>
+        <List title="Chemists" list={chemists}/>
+        <List title="Other" list={everyOneElse}/>
+    </article>
+)
 
 export default CommonList
